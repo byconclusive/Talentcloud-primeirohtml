8c2a1d2fc4180da79839635402bf065651b6280a
João Filho <joao.casfilho@gmail.com>
2023-09-28 17:35:33 -0300

    Att função com dois numeros

--- ds js01/calculadora2numer.js
+++ ds js01/calculadora2numer.js
@@ -1,10 +1,21 @@
+//Forma sem frases
+function contas1 (x, y){
+  console.log(`${x} + ${y} = ${x + y}`)
+  console.log(`${x} - ${y} = ${x - y}`)
+  console.log(`${x} * ${y} = ${x * y}`)
+  console.log(`${x} / ${y} = ${x / y}`)
+}
+
+contas1(10, 56)
 
  
-function contas(num1, num2){
+//Forma com frases
+function contas2(num1, num2){
   console.log("A soma dos números é: " , num1 + num2);
   console.log("A subtração entre os números é: ", num1 - num2);
   console.log("A multiplcação entre os números é: ", num1 * num2);
   console.log("O resultado da divisão é: ", num1 / num2);
 }
 
-contas(10, 56)
+contas2(10, 56)
+
