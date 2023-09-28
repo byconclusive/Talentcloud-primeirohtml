5d7e4cff6b00852af044d09b57650f7ae7bf40f3
João Filho <joao.casfilho@gmail.com>
2023-09-28 16:26:14 -0300

    Atividade conexão com JS

---
+++ ds js01/script.js
@@ -0,0 +1,6 @@
+function conexao(){
+    console.log("Conexão realizada com sucesso!")
+    console.log("Conexão realizada com sucesso!")
+    console.log("Conexão realizada com sucesso!")
+}
+conexao()
