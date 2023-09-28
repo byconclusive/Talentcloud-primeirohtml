2bb83d8b5be8d9531a88e7b78b57a91de06e6a44
João Filho <joao.casfilho@gmail.com>
2023-09-28 17:26:49 -0300

    Atividade função de dois numeros

---
+++ ds js01/calculadora2numer.js
@@ -0,0 +1,10 @@
+
+ 
+function contas(num1, num2){
+  console.log("A soma dos números é: " , num1 + num2);
+  console.log("A subtração entre os números é: ", num1 - num2);
+  console.log("A multiplcação entre os números é: ", num1 * num2);
+  console.log("O resultado da divisão é: ", num1 / num2);
+}
+
+contas(10, 56)
